3d8a736e10f0d4cdc1bb8d15de78e3becd434edc
Kael Santini <kaelsantini@hotmail.com>
2020-06-03 09:46:47 -0300

    fixing i18n labels

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -33,7 +33,7 @@ class PayTypeSelector extends React.Component {
           <select id="order_pay_type" onChange={this.onPayTypeSelected} name="order[pay_type]">
             <option value="">{I18n.t("orders.form.pay_prompt_html")}</option>
             <option value="Check">{I18n.t("orders.form.pay_types.check")}</option>
-            <option value="Credit card">{I18n.t("orders.form.pay_types.credit_cart")}</option>
+            <option value="Credit card">{I18n.t("orders.form.pay_types.credit_card")}</option>
             <option value="Purchase order">{I18n.t("orders.form.pay_types.purchase_order")}</option>
           </select>
         </div>
